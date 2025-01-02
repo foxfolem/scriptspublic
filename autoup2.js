e3e0c56e40672b942779defdaa7cbd363f4f1956
foxfolem <143623853+foxfolem@users.noreply.github.com>
2025-01-02 12:57:27 -0300

    Update autoup2.js

--- autoup2.js
+++ autoup2.js
@@ -91,7 +91,6 @@ async function start(){
                             }
                         }
                     }
-                    break;
                 }
             }
             await timer(200);
